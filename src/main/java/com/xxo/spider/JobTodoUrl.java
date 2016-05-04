5e3ea597592d1baf233b220585bb65611e334825
jasonTangxd <jason.tangxd@gmail.com>
2016-05-05 00:21:35 +0800

    网络爬虫，jOB调度

---
+++ src/main/java/com/xxo/spider/JobTodoUrl.java
@@ -0,0 +1,25 @@
+package com.xxo.spider;
+
+import com.xxo.spider.utils.RedisUtils;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.util.List;
+
+/**
+ * Created by xiaoxiaomo on 2016/5/4.
+ */
+public class JobTodoUrl implements Job {
+
+    RedisUtils redisUtils = new RedisUtils() ;
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        List<String> lrange = redisUtils.lrange(RedisUtils.start_url, 0, 1);
+        for (String s : lrange) {
+            redisUtils.add( RedisUtils.heightkey , s );
+        }
+        //System.out.println("---------");
+
+    }
+}
